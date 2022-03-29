f675365234ef701ddcc565e6fb7582622d52b1ca
Pau Brinkhaus <pau_b@protonmail.com>
2022-03-29 21:49:56 +0200

    001

--- id-001/src/main.rs
+++ id-001/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fm1(1000));
+}
+
+fn fm1(mut upper_bound: usize) -> usize {
+    let mut sum: usize = 0;
+    while upper_bound > 0 {
+        upper_bound -= 1;
+        if upper_bound % 5 == 0 || upper_bound % 3 == 0 {
+            // println!("{}", upper_bound);
+            sum += upper_bound;
+        }
+    }
+    sum
 }
