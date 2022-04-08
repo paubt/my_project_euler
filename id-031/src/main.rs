b70f200af84a5df70753979594eae0bca9f4d7b0
Pau Brinkhaus <pau_b@protonmail.com>
2022-04-08 22:42:05 +0200

    031 idea is gecatched

---
+++ id-031/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", coin_sums(200));
+}
+
+struct BergMoney {
+    one_pence: u32,
+    two_pence: u32,
+    five_pence: u32,
+    ten_pence: u32,
+    twenty_pence: u32,
+    fifty_pence: u32,
+    one_pound: u32,
+    two_pound: u32,
+}
+
+fn coin_sums(target_amount: usize) -> usize {
+    0
+}
